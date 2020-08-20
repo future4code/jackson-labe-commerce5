d5b172f531350d64178c2862d09987fa259d9d77
Paulo Moura <joaopaulo.034a@gmail.com>
2020-08-20 11:04:47 -0300

    componentes TripCard e Carrinho no App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,159 @@
 import React from 'react';
-import {Carrinho, ItemCarrinho} from './Components/Carrinho/Carrinho'
+
+import BlackHole from "./accets/images/blackhole.jpg"
+import Caprica from "./accets/images/caprica.jpg"
+import Darillium from "./accets/images/darillium.jpg"
+import Gallifrey from "./accets/images/gallifrey.jpg"
+import Krypton from "./accets/images/krypton.jpg"
+import Pandora from "./accets/images/pandora.jpg"
+import Tanaris from "./accets/images/tanaris.png"
+import Mars from "./accets/images/mars.jpg"
+
+import TripCard from "./accets/Components/TripCard/TripCard"
+import {Carrinho, ItemCarrinho} from './accets/Components/Carrinho/Carrinho'
 import './App.css';
 
-class App extends React.Component {
 
+const trip = [
+  {
+    id: 1,
+    planetImage: {BlackHole}, 
+    planetName: "Buraco Negro",
+    package: "3 dias estelares",
+    price: 1000,
+  },
+  {
+    id: 2,
+    planetImage: {BlackHole}, 
+    planetName: "Buraco Negro",
+    package: "5 dias estelares",
+    price: 1400,
+  },
+  {
+    id: 3,
+    planetImage:{Caprica},
+    planetName: "Caprica",
+    package: "3 dias estelares",
+    price: 400,
+  },
+  {
+    id: 4,
+    planetImage:{Caprica},
+    planetName: "Caprica",
+    package: "5 dias estelares",
+    price: 550,
+  },
+  {
+    id: 5,
+    planetImage:{Darillium},
+    planetName: "Darillium",
+    package: "3 dias estelares",
+    price: 750,
+  },
+  {
+    id: 6,
+    planetImage:{Darillium},
+    planetName: "Darillium",
+    package: "5 dias estelares",
+    price: 900,
+  },
+  {
+    id: 7,
+    planetImage:{Gallifrey},
+    planetName: "Gallifrey",
+    package: "3 dias estelares",
+    price: 1200,
+  },
+  {
+    id: 8,
+    planetImage:{Gallifrey},
+    planetName: "Gallifrey",
+    package: "5 dias estelares",
+    price: 1900,
+  },
+  {
+    id: 9,
+    planetImage:{Krypton},
+    planetName: "Krypton",
+    package: "3 dias estelares",
+    price: 600,
+  },
+  {
+    id: 10,
+    planetImage:{Krypton},
+    planetName: "Krypton",
+    package: "5 dias estelares",
+    price: 850,
+  },
+  {
+    id: 11,
+    planetImage:{Pandora},
+    planetName: "Pandora",
+    package: "3 dias estelares",
+    price: 1100,
+  },
+  {
+    id: 12,
+    planetImage:{Pandora},
+    planetName: "Pandora",
+    package: "5 dias estelares",
+    price: 1600,
+  },
+  {
+    id: 13,
+    planetImage:{Tanaris},
+    planetName: "Tanaris",
+    package: "3 dias estelares",
+    price: 500,
+  },
+  {
+    id: 14,
+    planetImage:{Tanaris},
+    planetName: "Tanaris",
+    package: "5 dias estelares",
+    price: 700,
+  },
+  {
+    id: 15,
+    planetImage:{Mars},
+    planetName: "Marte",
+    package: "3 dias estelares",
+    price: 100,
+  },
+  {
+    id: 16,
+    planetImage:{Mars},
+    planetName: "Marte",
+    package: "5 dias estelares",
+    price: 125,
+  }
+]
+
+//configurei as props do componente TripCard
+const tripGrid = trip.map((item) => {
+  return (
+    <TripCard
+    planetImage={item.planetImage}
+    planetName={item.planetName}
+    package={item.package}
+    price={item.price}
+    //addToCart={/*função add ao carrinho*/}
+    />
+  );
+})
+
+class App extends React.Component {
 
 
   //Aqui sera armazenado a lista de produtos
   //selecionados na pagina de produtos
   state = {
+    id: trip.id,
+    planetImage: trip.planetImage,
+    planetName: trip.planetName,
+    package: trip.package,
+    price: trip.price,
+
     listaDeItems: [
       {
         id: 1,
@@ -94,6 +239,7 @@ render(){
 
   return (
     <div className="App">
+      {tripGrid}
       {/* imprime o componente Carrinho com a lista de items criada no render */}
       <Carrinho valorTotal={this.state.valorTotal} item={itemsCarrinho}/>
     </div>
