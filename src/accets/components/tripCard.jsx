c385843f456145ef22a4ecee729cb6a20350f5f0
Mila <camilasouza@apples-MacBook-Pro.local>
2020-08-20 09:13:00 -0300

    possivel estilização, mas ainda não consigo visualizar

--- src/accets/components/tripCard.jsx
+++ src/accets/components/tripCard.jsx
@@ -1,23 +1,35 @@
 import React from "react";
+import styled from "styled-components";
 
-const TripCard = {
+const PlanetCard = styled.span`
+    border: thin dotted blueviolet;
+    padding: 0.5vw;
+    background-color: lightsteelblue;
+    color: darkslateblue;
+`
+const PlanetImage = styled.image`
+    max-width: 150px;
+    margin: 0 auto;
+`
+const AddButton = styled.button`
+    background-color: darkblue;
+    color: lightyellow;
+    padding: 1vw;
+    margin: 0 auto;
+    :hover{
+        background-color: red;
+    }
+`
 
-}
-const PlanetImage = {
-
-}
-const AddButton = {
-
-}
-
-export default function(props) {
+export default function TripCard(props) {
     const {trip, addToCart} = props;
     return (
-        <TripCard>
+        <PlanetCard>
             <PlanetImage src= {trip.planetImage} alt="Visite nosso planeta" />
                 <p>{trip.planetName}</p>
+                <p>{trip.package}</p>
                 <p>§$ {trip.price}</p>
-                <AddButton onClick={}>Adicionar ao foguete</AddButton>
-        </TripCard>
+                <AddButton onClick={addToCart}>Adicionar ao foguete</AddButton>
+        </PlanetCard>
     )
 }
