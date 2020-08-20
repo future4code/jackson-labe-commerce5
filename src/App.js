71f88337e42e91ea79767c0491049381bab931e9
Mila <camilasouza@apples-MacBook-Pro.local>
2020-08-20 08:07:17 -0300

    subindo umas correções na const trip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
- labespace-Camila
 import tripCard from "./accets/components/tripCard.jsx";
 import BlackHole from "./accets/images/blackhole.jpg";
 import Caprica from "./accets/images/caprica.jpg";
@@ -10,57 +9,120 @@ import Pandora from "./accets/images/pandora.jpg";
 import Tanaris from "./accets/images/tanaris.png";
 import Mars from "./accets/images/mars.jpg";
 import logo from './logo.svg';
-=======
-master
+
 import './App.css';
 
 const trip = [{
     id: 1,
     planetImage: {BlackHole}, 
     planetName: "Buraco Negro",
+    pacote: "3 dias estelares",
     price: 1000,
   },
   {
     id: 2,
-    planetImage:{Caprica},
-    planetName: "Caprica",
-    price: 400,
+    planetImage: {BlackHole}, 
+    planetName: "Buraco Negro",
+    pacote: "5 dias estelares",
+    price: 1400,
   },
   {
     id: 3,
-    planetImage:{Darillium},
-    planetName: "Darillium",
-    price: 750,
+    planetImage:{Caprica},
+    planetName: "Caprica",
+    pacote: "3 dias estelares",
+    price: 400,
   },
   {
     id: 4,
-    planetImage:{Gallifrey},
-    planetName: "Gallifrey",
-    price: 1200,
+    planetImage:{Caprica},
+    planetName: "Caprica",
+    pacote: "5 dias estelares",
+    price: 550,
   },
   {
     id: 5,
-    planetImage:{Krypton},
-    planetName: "Krypton",
-    price: 600,
+    planetImage:{Darillium},
+    planetName: "Darillium",
+    pacote: "3 dias estelares",
+    price: 750,
   },
   {
     id: 6,
-    planetImage:{Pandora},
-    planetName: "Pandora",
-    price: 1100,
+    planetImage:{Darillium},
+    planetName: "Darillium",
+    pacote: "5 dias estelares",
+    price: 900,
   },
   {
     id: 7,
-    planetImage:{Tanaris},
-    planetName: "Tanaris",
-    price: 500,
+    planetImage:{Gallifrey},
+    planetName: "Gallifrey",
+    pacote: "3 dias estelares",
+    price: 1200,
   },
   {
     id: 8,
+    planetImage:{Gallifrey},
+    planetName: "Gallifrey",
+    pacote: "5 dias estelares",
+    price: 1900,
+  },
+  {
+    id: 9,
+    planetImage:{Krypton},
+    planetName: "Krypton",
+    pacote: "3 dias estelares",
+    price: 600,
+  },
+  {
+    id: 10,
+    planetImage:{Krypton},
+    planetName: "Krypton",
+    pacote: "5 dias estelares",
+    price: 850,
+  },
+  {
+    id: 11,
+    planetImage:{Pandora},
+    planetName: "Pandora",
+    pacote: "3 dias estelares",
+    price: 1100,
+  },
+  {
+    id: 12,
+    planetImage:{Pandora},
+    planetName: "Pandora",
+    pacote: "5 dias estelares",
+    price: 1600,
+  },
+  {
+    id: 13,
+    planetImage:{Tanaris},
+    planetName: "Tanaris",
+    pacote: "3 dias estelares",
+    price: 500,
+  },
+  {
+    id: 14,
+    planetImage:{Tanaris},
+    planetName: "Tanaris",
+    pacote: "5 dias estelares",
+    price: 700,
+  },
+  {
+    id: 15,
     planetImage:{Mars},
     planetName: "Marte",
+    pacote: "3 dias estelares",
     price: 100,
+  },
+  {
+    id: 16,
+    planetImage:{Mars},
+    planetName: "Marte",
+    pacote: "5 dias estelares",
+    price: 125,
   }
 ]
 
